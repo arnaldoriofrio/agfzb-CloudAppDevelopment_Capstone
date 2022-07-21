e9d4850661308725613f9cdbf2da568a3545d302
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-07-21 10:49:31 -0500

    Update get-dealerships.js
    2 commit

--- functions/get-dealerships.js
+++ functions/get-dealerships.js
@@ -11,12 +11,48 @@ async function main(params) {
           authenticator: authenticator
       });
       cloudant.setServiceUrl(params.COUCH_URL);
-      try {
-        let db = await cloudant.use("dealerships");
-        let ldealerships = await db.list({include_docs: true})
+      /*
+      *try {
+      *  let db = await cloudant.use("dealerships");
+      *  let ldealerships = await db.list({include_docs: true})
+      *
+      * return { ldealerships };
+      *} catch (error) {
+      *    return { error: error.description };
+      *}
+      */
+     const state = params.state;
+     const dealerId = params.dealerId
 
-        return { ldealerships };
-      } catch (error) {
-          return { error: error.description };
-      }
+     try {
+         const db = await cloudant.use('dealerships');
+         const dealerships = await db.list({include_docs: true})
+
+         if(state) {
+             let filter = state.replace(/"/g, '');
+             dealerships.rows = dealerships.rows.filter(({doc}) => doc.state === filter )
+         }
+
+         if(dealerId) {
+             let filter = Number(dealerId)
+             dealerships.rows = dealerships.rows.filter(({doc}) => doc.id === filter )
+         }
+
+         return {
+             result: dealerships.rows.map(({doc}) => ({
+                 id: doc.id,
+                 city: doc.city,
+                 state: doc.state,
+                 st: doc.st,
+                 address: doc.address,
+                 zip: doc.zip,
+                 lat: doc.lat,
+                 long: doc.long
+             }))
+         };
+     } catch (error) {
+         console.log(error)
+         return { error: error.description };
+     }
+      
 }
