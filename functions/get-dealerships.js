ab1d10bedd65e495c1184476d41bf6a51c9b7790
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-07-31 16:02:00 -0500

    Update get-dealerships.js
    10 commit

--- functions/get-dealerships.js
+++ functions/get-dealerships.js
@@ -1,102 +1,54 @@
 /**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+
+/**
  * Get all dealerships
  */
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-import requests
-
-
-async function main(params):
-    const cloudant = Cloudant({
-          url: params.COUCH_URL,
-          plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
-    });
-
-     const state = params.state;
-     const dealerId = params.dealerId
-
-     try {
-         const db = await cloudant.use('dealerships');
-         const dealerships = await db.list({include_docs: true})
-
-         if(state) {
-             let filter = state.replace(/"/g, '');
-             dealerships.rows = dealerships.rows.filter(({doc}) => doc.state === filter )
-         }
-
-         if(dealerId) {
-             let filter = Number(dealerId)
-             dealerships.rows = dealerships.rows.filter(({doc}) => doc.id === filter )
-         }
-
-         return {
-             result: dealerships.rows.map(({doc}) => ({
-                 id: doc.id,
-                 city: doc.city,
-                 state: doc.state,
-                 st: doc.st,
-                 address: doc.address,
-                 zip: doc.zip,
-                 lat: doc.lat,
-                 long: doc.long,
-                 short_name: doc.short_name,
-                 full_name: doc.full_name
-             }))
-         };
-     } catch (error) {
-         console.log(error)
-         return { error: error.description };
-     }
-      
-}
-
-
-/*
-const { CloudantV1 } = require('@ibm-cloud/cloudant');
-const { IamAuthenticator } = require('ibm-cloud-sdk-core');
-
 async function main(params) {
-      const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
-      const cloudant = CloudantV1.newInstance({
-          authenticator: authenticator
-      });
-      cloudant.setServiceUrl(params.COUCH_URL);
-      
-     const state = params.state;
-     const dealerId = params.dealerId
+    
+    const { CloudantV1 } = require('@ibm-cloud/cloudant');
+    const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
-     try {
-         const db = await cloudant.use('dealerships');
-         const dealerships = await db.list({include_docs: true})
+	/*return { body: {'message':'Hello World '}};*/
 
+     
+    const state = params.state;
+    const dealerId = params.dealerId
+
+
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = CloudantV1.newInstance({ authenticator: authenticator});
+    cloudant.setServiceUrl(params.COUCH_URL);
+
+    try {
+         const dealerships = await cloudant.postAllDocs({db: 'dealerships',  includeDocs: true})
          if(state) {
-             let filter = state.replace(/"/g, '');
-             dealerships.rows = dealerships.rows.filter(({doc}) => doc.state === filter )
+             let matriz=dealerships.result.rows
+             let dict=matriz.filter(x=>x.doc.state==state)
+             cuerpo={ body: { rows: dict} }
          }
-
-         if(dealerId) {
-             let filter = Number(dealerId)
-             dealerships.rows = dealerships.rows.filter(({doc}) => doc.id === filter )
+         else if(dealerId) {
+             let matriz=dealerships.result.rows
+             let dict=matriz.filter(x=>x.doc.id==Number(dealerId))
+             cuerpo={ body: { rows: dict} }
          }
-
-         return {
-             result: dealerships.rows.map(({doc}) => ({
-                 id: doc.id,
-                 city: doc.city,
-                 state: doc.state,
-                 st: doc.st,
-                 address: doc.address,
-                 zip: doc.zip,
-                 lat: doc.lat,
-                 long: doc.long,
-                 short_name: doc.short_name,
-                 full_name: doc.full_name
-             }))
-         };
+         else {     
+            let dict=dealerships.result
+            cuerpo={ body: dict }
+         }        
+         return cuerpo
+         
      } catch (error) {
          console.log(error)
          return { error: error.description };
-     }
-      
+         
+    }
+     
 }
-*/
