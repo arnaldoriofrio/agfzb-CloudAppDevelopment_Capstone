9cd2174ab676a35248ee57d7336482f2ec03a977
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-07-31 16:09:33 -0500

    Update restapis.py
    3 commit

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -54,9 +54,11 @@ def get_dealers_from_cf(url, **kwargs):
     return results
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-# def get_dealer_by_id_from_cf(url, dealerId):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a DealerView object list
+
+# def get_dealer_by_id_from_cf(url, dealerId):
+
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
