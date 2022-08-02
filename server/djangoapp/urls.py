b51c5646b60ea73d14001b15a7b6c2e912f26840
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-01 20:56:26 -0500

    Update urls.py
    8 commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index')
 
     # path for add a review view
-    path(route='dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details')
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details')
     
-    path(route='dealer/<int:dealer_id>/add_review_request', view=views.add_review_request, name='Add Review POST')
+    path('dealer/<int:dealer_id>/add_review_request', views.add_review_request, name='Add Review POST')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
