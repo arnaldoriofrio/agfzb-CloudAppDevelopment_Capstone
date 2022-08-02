89a4769defbc357e977fcdc05c50c1e5592b9474
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-02 16:22:21 -0500

    Update admin.py
    12 commit

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -16,6 +16,12 @@ class CarMakeAdmin(admin.ModelAdmin):
     fields = ['name', 'description']
     inlines = [CarModelInline]
 
+ # DealerReview class with CarModelInline
+class DealerReviewAdmin(admin.ModelAdmin):
+    fields = ['id', 'dealership', 'name', 'purchase', 'review', 'purchase_date', 'car_make', 'car_model', 'car_year', 'sentiment']
+    
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(DealerReview, DealerReviewAdmin)
+
