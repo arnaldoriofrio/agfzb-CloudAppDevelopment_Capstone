947fb08561bf88efa15a53088e7ef79b89513b3d
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-02 08:39:48 -0500

    Update urls.py
    8 commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -25,10 +25,10 @@ urlpatterns = [
     path('logout/', views.logout_request, name='logout'),
 
     # path for dealer reviews view
-    path(route='', view=views.get_dealerships, name='index')
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for add a review view
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details')
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
     
     path('dealer/<int:dealer_id>/add_review_request', views.add_review, name='add_review)
 
