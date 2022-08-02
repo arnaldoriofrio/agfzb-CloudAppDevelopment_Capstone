7d3f17a48df72d370502bb7415c84eb8cd246e44
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-02 09:41:01 -0500

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 from .models import CarModel
 # from .restapis import related methods
-from .restapis import get_dealers_from_cf, analyze_review_sentiments, get_dealer_reviews_from_cf, get_dealers_by_id_from_cf, get_dealer_by_state_from_cf
+from .restapis import get_dealers_from_cf, analyze_review_sentiments, get_dealer_reviews_from_cf, get_dealers_by_id_from_cf, get_dealers_by_state_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
