3e19c8edf4990f8059d1bb424617c15b85952afc
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-03 15:55:53 -0500

    Update models.py
    7 commit

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -130,7 +130,7 @@ class CarDealer:
     def __str__(self):
         return "Dealer name: " + self.full_name
 
-   class DealerReview:
+class DealerReview:
     def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
         self.dealership = dealership
         self.name = name
