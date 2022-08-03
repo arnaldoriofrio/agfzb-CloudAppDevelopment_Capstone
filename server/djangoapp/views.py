102ac05c2ec592f50b273d827cca44d34170d496
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-02 21:36:59 -0500

    Update views.py
    9 commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -91,7 +91,7 @@ def get_dealerships(request):
         # Concat all dealer's short name
         # dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        text["dealerships"] = dealerships
+        context["dealerships"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
