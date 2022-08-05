6dcbdf0d9fc628b47f462a3c0a641aae579bcc14
arnaldoriofrio <62032451+arnaldoriofrio@users.noreply.github.com>
2022-08-04 20:58:28 -0500

    Update post-review.py
    Last commit

--- functions/post-review.py
+++ functions/post-review.py
@@ -6,37 +6,56 @@
 #
 # @return The output of this action, which must be a JSON object.
 #
-#
-import json
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-import requests
+def main(params):
 
-
-def main(dict):
-    databaseName = "reviews"
+    import json
+    from ibmcloudant.cloudant_v1 import Document, CloudantV1
+    from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+    from ibm_cloud_sdk_core import ApiException
 
     try:
-        client = Cloudant.iam(
-            account_name=dict["COUCH_USERNAME"],
-            api_key=dict["IAM_API_KEY"],
-            connect=True,
-        )
+        #print(params)
+        authenticator = IAMAuthenticator(params['CLOUDANT_APIKEY'])
+        service = CloudantV1(authenticator=authenticator)
+        service.set_service_url(params['CLOUDANT_URL'])
+        
+        reviews_doc = Document(
+        #id=params['id'],
+        name=params['name'],
+        dealership=params['dealership'],
+        review=params['review'],
+        purchase=params['purchase'],
+        purchase_date=params['purchase_date'],
+        car_make=params['car_make'],
+        car_model=params['car_model'],
+        car_year=params['car_year'])
+        
+        response = service.post_document(db='reviews', document=reviews_doc).get_result()
+        #print(response)
+        #return reviews_doc
+        
+        return response
 
-        db = client[databaseName]
+#    try:
+#        authenticator = IAMAuthenticator('l5LNBMImpBgQmrLUmuqlCmG4w0ZbnbP7-8Prd48hVzZl')
+#        service = CloudantV1(authenticator=authenticator)
+#        service.set_service_url('https://c4a1e7aa-3d04-493c-897d-f71e393bbeb1-bluemix.cloudantnosqldb.appdomain.cloud')
+#        
+#        data=service.post_all_docs(db="reviews", include_docs=True).get_result()
+#        dealerId=15
+#        dealerId = dealerId.strip('"') if type(dealerId) == str else dealerId
+        
+#        if dealerId:
+#            response = list(filter(lambda r: r['doc']['dealership'] == dealerId, data['rows']))
+        
+#        return {
+#            "body": {"rows":response}
+#        }
 
-        data = dict["review"]
-        doc = db.create_document(data)
-        doc.save()
+    except ApiException as ae:
+        print("Method failed")
+        print(" - status code: " + str(ae.code))
+        print(" - error message: " + ae.message)
+        if ("reason" in ae.http_response.json()):
+            print(" - reason: " + ae.http_response.json()["reason"])
 
-        return {
-            "result": data
-        }
-    except CloudantException as ce:
-        print("unable to connect")
-        return {"error": ce}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
